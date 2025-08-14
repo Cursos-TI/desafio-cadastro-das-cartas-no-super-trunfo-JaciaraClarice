07198da9c7e7b120d5b5e2d54061e8da0138f078
JaciaraClarice <jaciaraclarice368@gmail.com>
2025-08-14 20:03:44 +0000

    Novo commit

--- DesafioSuperTrunfo-cadastrodascartas/DesafioSuperTrunfo-cadastrodascartas.c
+++ DesafioSuperTrunfo-cadastrodascartas/DesafioSuperTrunfo-cadastrodascartas.c
@@ -1,54 +1,75 @@
 #include <stdio.h>
 
-int main() {
+int main () {
+
 
     // Inicialização das variáveis da primeira carta
 
     char estado1[15] = "Acre";
+    int carta1 = 1;
     char codigodacarta1[5] = "A01";
     char nomedacidade1[15] = "Rio Branco";
-    float populacao1 = 880361;
+    int populacao1 = 880361;
     float area1 = 1059;
     float pib1 = 21374;
     int pontosturisticos1 = 9;
 
     printf("Desafio Super Trunfo Nível Novato \n");
     
-    printf("\nCadastro Básico: \n");
-
-    // Entrada dos dados da primeira carta
-
-    printf("\nCarta 1: \n");
-    printf("Código da carta: %s\n", codigodacarta1);
-    printf("Estado: %s\n", estado1);
-    printf("Nome da cidade: %s\n", nomedacidade1);
-    printf("População: %2.f\n", populacao1);
-    printf("Área: %2.f km² \n", area1);
-    printf("PIB: %2.f\n", pib1);
-    printf("Pontos Turísticos: %d\n", pontosturisticos1);
-
-   
-   // Inicialização das variáveis da segunda carta
+    // Inicialização das variáveis da segunda carta
 
     int carta2 = 2;
     char estado2[20] = "Belém";
     char codigodacarta2[5] = "A02";
     char nomedacidade2[30] = "Pará";
-    float populacao2 = 1398581;
+    int populacao2 = 1398581;
     float area2 = 152531;
     float pib2 = 30835;
     int pontosturisticos2 = 6;
 
-    // Entrada dos dados da segunda carta
 
-    printf("\nCarta 2: \n");
-    printf("Código da carta: %s\n", codigodacarta2);
-    printf("Estado: %s\n", estado2);
-    printf("Nome da cidade: %s\n", nomedacidade2);
-    printf("População: %2.f\n", populacao2);
-    printf("Área: %2.f km² \n", area2);
-    printf("PIB: %2.f\n", pib2);
-    printf("Pontos Turísticos: %d\n", pontosturisticos2);
+    // Cadastro básico da primeira carta
+    printf("Cadastro da primeira carta:\n");
+    printf("Populacão: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontosturisticos1);
+
+
+    // Cadastro básico da segunda carta
+    printf("\nCadastro da segunda carta:\n");
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontosturisticos2);
+
+    // Exibindo os dados cadastrados
+    printf("\nDados da Primeira Carta:\n");
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turísticos: %d\n", pontosturisticos1);
+
+    printf("\nDados da Segunda Carta:\n");
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos turísticos: %d\n", pontosturisticos2);
 
     return 0;
+
 }
